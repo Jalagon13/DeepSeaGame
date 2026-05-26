2877545482f1e3b78e323c3d6c8797cf5809e560
Jalagon <A1ag0nJus1in@gmail.com>
2026-05-26 11:18:16 +0900

    Old biome background deleted

--- Assets/_UntitledDeepSeaGame/_Scripts/_Generation/GenerationCommons.cs
+++ Assets/_UntitledDeepSeaGame/_Scripts/_Generation/GenerationCommons.cs
@@ -25,32 +25,4 @@ namespace UntitledDeepSeaGame
         ForegroundTilemap,
         BackgroundTilemap
     }
-
-    [Serializable]
-    public class BiomeBackgroundLayer
-    {
-        [Tooltip("Name of the biome layer (e.g., Underground, Cave, Space)")]
-        public string layerName;
-
-        [Tooltip("The repeating background texture sprite. Note: Sprite wrap mode must be set to Repeat in import settings.")]
-        public Sprite backgroundSprite;
-
-        [Tooltip("Lower bounds of this layer in world space coordinates (Y coordinate)")]
-        public float minY;
-
-        [Tooltip("Upper bounds of this layer in world space coordinates (Y coordinate)")]
-        public float maxY;
-
-        [Range(0f, 1f), Tooltip("Horizontal parallax scrolling speed factor (0 = moves fully with camera, 1 = locked to world)")]
-        public float parallaxFactorX = 0.5f;
-
-        [Tooltip("Sorting order for the background SpriteRenderer (should be lower than tilemaps, e.g. -100)")]
-        public int sortingOrder = -100;
-
-        [HideInInspector]
-        public SpriteRenderer spriteRenderer;
-
-        [HideInInspector]
-        public GameObject layerGameObject;
-    }
 }
