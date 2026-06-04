4909426dd2c957f2ece98e7df3403a2b1a14d306
Jalagon <A1ag0nJus1in@gmail.com>
2026-06-04 16:43:12 +0900

    Crafting system started

---
+++ Assets/_UntitledDeepSeaGame/_Scripts/_UI/CraftItemPanelUI.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UntitledDeepSeaGame
+{
+    public class CraftItemPanelUI : MonoBehaviour
+    {
+        [SerializeField] private GameObject _contentPanel;
+        [SerializeField] private Image _outputItemImage;
+        [SerializeField] private TextMeshProUGUI _outputItemNameText;
+        [SerializeField] private GameObject _ingredientListSectionUI;
+        [SerializeField] private IngredientPanelUI _ingredientPanelUIPrefab;
+
+        [Header("Craft Button")]
+        [SerializeField] private Image _craftButtonCantCraftOverlay;
+        [SerializeField] private TextMeshProUGUI _craftButtonText;
+        [SerializeField] private float _craftingDuration = 2f;
+
+        private List<IngredientPanelUI> _ingredientList;
+
+        private RecipeSO _currentRecipe;
+
+        private void Awake()
+        {
+            Hide();
+        }
+
+        private void Start()
+        {
+            InventoryManager.Instance.OnInventoryChanged += UpdateCraftButtonOverlay;
+        }
+
+
+        private void OnDestroy()
+        {
+            InventoryManager.Instance.OnInventoryChanged -= UpdateCraftButtonOverlay;
+        }
+
+        public void UpdatePanel(RecipeSO recipe)
+        {
+            _currentRecipe = recipe;
+            _craftButtonCantCraftOverlay.enabled = false;
+
+            PopulateCraftItemPanelUI(recipe);
+            UpdateCraftButtonOverlay();
+            Show();
+        }
+
+        private void PopulateCraftItemPanelUI(RecipeSO recipe)
+        {
+            _outputItemImage.sprite = recipe.OutputItem.InventoryIcon;
+            _outputItemNameText.text = recipe.OutputItem.ItemName;
+
+            ClearIngredientListSectionUI();
+            PopulateIngredientListSectionUI(recipe);
+        }
+
+        private void ClearIngredientListSectionUI()
+        {
+            foreach (Transform child in _ingredientListSectionUI.transform)
+            {
+                Destroy(child.gameObject);
+            }
+
+            if (_ingredientList == null)
+            {
+                _ingredientList = new();
+            }
+            else
+            {
+                _ingredientList.Clear();
+            }
+        }
+
+        private void PopulateIngredientListSectionUI(RecipeSO recipe)
+        {
+            for (int i = 0; i < recipe.Requirements.Count; i++)
+            {
+                ItemRequirement itemRequirement = recipe.Requirements[i];
+                IngredientPanelUI ingredientPanelUI = Instantiate(_ingredientPanelUIPrefab.gameObject, _ingredientListSectionUI.transform).GetComponent<IngredientPanelUI>();
+                ingredientPanelUI.Setup(itemRequirement);
+
+                _ingredientList.Add(ingredientPanelUI);
+            }
+        }
+
+        public void OnCraftButtonPressed()
+        {
+            foreach (IngredientPanelUI ingredientPanelUI in _ingredientList)
+            {
+                if (!ingredientPanelUI.HasIngredient)
+                {
+                    return;
+                }
+            }
+            HandleCraftingComplete();
+            UpdateCraftButtonOverlay();
+        }
+
+        private void HandleCraftingComplete()
+        {
+            foreach (var item in _currentRecipe.Requirements)
+            {
+                InventoryManager.Instance.RemoveItem(item.Item, item.Amount);
+            }
+
+            InventoryManager.Instance.AddItem(_currentRecipe.OutputItem, _currentRecipe.OutputAmount);
+        }
+
+
+        private void UpdateCraftButtonOverlay()
+        {
+            if (_ingredientList == null || _ingredientList.Count == 0)
+            {
+                _craftButtonCantCraftOverlay.enabled = true;
+                return;
+            }
+
+            foreach (IngredientPanelUI ingredientPanelUI in _ingredientList)
+            {
+                if (!ingredientPanelUI.HasIngredient)
+                {
+                    _craftButtonCantCraftOverlay.enabled = true;
+                    return;
+                }
+            }
+
+            _craftButtonCantCraftOverlay.enabled = false;
+        }
+
+        public void Show()
+        {
+            _contentPanel.SetActive(true);
+        }
+
+        private void Hide()
+        {
+            _contentPanel.SetActive(false);
+        }
+    }
+}
---
+++ Assets/_UntitledDeepSeaGame/_Scripts/_UI/IngredientPanelUI.cs
@@ -0,0 +1,48 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UntitledDeepSeaGame
+{
+    public class IngredientPanelUI : MonoBehaviour
+    {
+        [SerializeField] private Image _iconImage;
+        [SerializeField] private TextMeshProUGUI _nameText;
+        [SerializeField] private TextMeshProUGUI _amountNeededText;
+        [SerializeField] private Image _notAvailableOverlay;
+
+        private ItemRequirement _itemReq;
+
+        public bool HasIngredient { get; private set; }
+
+        private void Start()
+        {
+            InventoryManager.Instance.OnInventoryChanged += UpdateIngredientStatus;
+        }
+
+        private void OnDestroy()
+        {
+            InventoryManager.Instance.OnInventoryChanged -= UpdateIngredientStatus;
+        }
+
+        public void Setup(ItemRequirement itemRequirement)
+        {
+            _iconImage.sprite = itemRequirement.Item.InventoryIcon;
+            _nameText.text = itemRequirement.Item.ItemName;
+            _itemReq = itemRequirement;
+
+            UpdateIngredientStatus();
+        }
+
+        private void UpdateIngredientStatus()
+        {
+            if (_itemReq.Item == null) return;
+
+            int currentAmount = InventoryManager.Instance.GetItemAmount(_itemReq.Item);
+            _amountNeededText.text = $"{currentAmount}/{_itemReq.Amount}";
+            HasIngredient = InventoryManager.Instance.HasItemAmount(_itemReq.Item, _itemReq.Amount);
+            _notAvailableOverlay.enabled = !HasIngredient;
+        }
+    }
+}
