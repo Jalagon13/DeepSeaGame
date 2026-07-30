e935964f579cdd7ea38e968abf510caadb63b668
Jalagon <A1ag0nJus1in@gmail.com>
2026-07-30 09:34:23 -0700

    Oxygen Warning done

--- Assets/DeepSeaGame/Scripts/UI/OxygenWarningUI.cs
+++ Assets/DeepSeaGame/Scripts/UI/OxygenWarningUI.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using DG.Tweening;
+using MoreMountains.Feedbacks;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -6,16 +9,25 @@ namespace DeepSeaGame
 {
     public class OxygenWarningUI : NetworkBehaviour
     {
-        private void Awake() 
+        [SerializeField] private float _displayTimer = 3f;
+        [SerializeField] private RectTransform _warningPanel;
+
+        [Header("Scale Pulse Settings")]
+        [SerializeField] private int _pulseCount = 2;
+        [SerializeField] private float _pulseScale = 1.5f;
+        [SerializeField] private float _pulseDuration = 0.4f;
+
+        private void Awake()
         {
+            DisableWarningPanel();
             Player.OnAnyPlayerSpawned += RegisterOxygenWarning;
         }
 
         public override void OnDestroy()
         {
             Player.OnAnyPlayerSpawned -= RegisterOxygenWarning;
-            
-            if(Player.Instance != null)
+
+            if (Player.Instance != null)
             {
                 Player.Instance.PlayerOxygenController.OnOxygenWarning -= OnOxygenWarning;
             }
@@ -30,10 +42,42 @@ namespace DeepSeaGame
 
         private void OnOxygenWarning()
         {
-            Debug.Log($"Oxygen Warning");
+            Debug.Log("Oxygen Warning");
             AudioManager.Instance.PlayOneShot(FMODEvents.Instance.OxygenWarningSFX, default);
+
+            EnableWarningPanel();
+            StopAllCoroutines();
+            StartCoroutine(DisableWarningPanelAfterDelay());
+        }
+
+        private IEnumerator DisableWarningPanelAfterDelay()
+        {
+            yield return new WaitForSeconds(_displayTimer);
+            DisableWarningPanel();
+        }
+
+        private void EnableWarningPanel()
+        {
+            _warningPanel.gameObject.SetActive(true);
+            _warningPanel.localScale = Vector3.one;
+
+            _warningPanel.DOKill();
+
+            Sequence pulseSequence = DOTween.Sequence();
             
+            for (int i = 0; i < _pulseCount; i++)
+            {
+                pulseSequence.Append(_warningPanel.DOScale(_pulseScale, _pulseDuration * 0.5f).SetEase(Ease.InOutSine));
+                pulseSequence.Append(_warningPanel.DOScale(1f, _pulseDuration * 0.5f).SetEase(Ease.InOutSine));
+            }
             
+            pulseSequence.Play();
+        }
+
+        private void DisableWarningPanel()
+        {
+            _warningPanel.DOKill();
+            _warningPanel.gameObject.SetActive(false);
         }
     }
 }
